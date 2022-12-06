bc3ee70ebbf6ef9f5b9acc4c9ad3985dd0eddaa0
JerichoNash <jerichonash@gmail.com>
2022-12-06 12:47:16 +0800

    modify promise1.js

--- js/promise1.js
+++ js/promise1.js
@@ -2,7 +2,7 @@ let doWorkPromise = function(job, timer){
     return new Promise((resolve, reject) =>{
         setTimeout(() =>{
             let now = new Date();
-            resolve(`完成工作 ${job} at ${now.toISOString()}`);
+            resolve(`完成工作 ${job} at ${now.toISOString()}`)
         }, timer);
     })
 }
@@ -10,10 +10,27 @@ let doWorkPromise = function(job, timer){
 let now = new Date();
 console.log(`工作開始 at ${now.toISOString()}`);
 let brushPromise = doWorkPromise(`刷牙`, 3000);
-brushPromise
+let eatbreafirst = doWorkPromise(`吃早餐`, 5000);
+let dohw = doWorkPromise(`寫功課`, 3000);
+
+doWorkPromise(`刷牙`, 3000)
+    .then((data) =>{
+        console.log(data);
+        return doWorkPromise(`吃早餐`, 5000);
+    })
+    .then((data) =>{
+        console.log(data);
+        return doWorkPromise(`寫功課`, 3000);
+    })
     .then((data) =>{
         console.log(data);
     })
     .catch((err) =>{
         console.error(`發生錯誤`, err);
-    });
+    })
+    
+
+
+
+
+
