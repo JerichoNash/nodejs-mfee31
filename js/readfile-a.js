866e6433d6c8cbbd15c3300f594b0f6b5885bf90
JerichoNash <jerichonash@gmail.com>
2022-12-10 15:50:21 +0800

    add quize1,2,3.js & modify readfile-a.js

--- js/readfile-a.js
+++ js/readfile-a.js
@@ -1,18 +1,18 @@
-const fs = require('fs');
+const fs = require("fs");
 // Promise 是一個表示非同步運算的最終完成或失敗的物件。
 let p = new Promise((resolve, reject) => {
-    // error-first callback
-      fs.readFile('test.txt', 'utf-8', (err, data) => {
-        if (err) {
-        // 如果 err 有值，表示有錯誤發生
-        // 這裡應該要處理錯誤
-          reject(err);
-        } else {
-        // 進來這裡，表示 err 是空的 (可能是 null)
-          resolve(data);
-        }
-      });
-    });
+  // error-first callback
+  fs.readFile("test.txt", "utf-8", (err, data) => {
+    if (err) {
+      // 如果 err 有值，表示有錯誤發生
+      // 這裡應該要處理錯誤
+      reject(err);
+    } else {
+      // 進來這裡，表示 err 是空的 (可能是 null)
+      resolve(data);
+    }
+  });
+});
 // async function doJob(){
 //     let data = await p;
 // }
@@ -20,12 +20,12 @@ let p = new Promise((resolve, reject) => {
 // dojob();
 
 // IIEF
-(async ()=> {
-    try{
-        let data = await p;
-        console.log('用await 拿到結果', data);
-    }catch(e){
-        console.log('catch 到錯誤', e);
-    }
+(async () => {
+  try {
+    let data = await p;
+    console.log("用await 拿到結果", data);
+  } catch (e) {
+    console.log("catch 到錯誤", e);
+  }
 })();
 // (function test() {})();
