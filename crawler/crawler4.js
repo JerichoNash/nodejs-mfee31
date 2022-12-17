c51df62bb605a9c3709cb5c1f2bed427d92489ba
JerichoNash <jerichonash@gmail.com>
2022-12-17 15:22:34 +0800

    modify crawler4.js

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -14,16 +14,21 @@ require("dotenv").config();
 // dotenv.config();
 
 (async () => {
-  let connection = await mysql2.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-  });
+  let connection;
+  // connection => undefined
   try {
+    connection = await mysql2.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PWD,
+      database: process.env.DB_NAME,
+    });
+
     let stockNo = await fs.readFile("stock.txt", "utf-8");
+    console.log(stockNo);
     let date = moment().format("YYYYMMDD");
+    console.log(date);
     let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
       params: {
         stockNo,
@@ -32,28 +37,29 @@ require("dotenv").config();
     });
 
     let rawData = response.data.data;
-    rawData.map((d) => {
-      // d=>本身也是一個陣列
-      // ['日期','成交股數','成交金額', '開盤價','最高價',   '最低價','收盤價', '漲跌價差','成交筆數']
-    //   console.log("before", d);
+    rawData.map(async (d) => {
+      // d => 本身也是一個陣列
+      // ["日期", "成交股數", "成交金額", "開盤價", "最高價","最低價", "收盤價","漲跌價差","成交筆數"],
+      // "43,684,491" => 移除 ,
+      // console.log('before', d);
       d = d.map((value) => {
         return value.replace(/[,X]/g, "");
       });
+      // '111/12/15' => 2022-12-15
+      // '111/12/15' => '1111215' + 19110000 => 20221215
+      let dt = parseInt(d[0].replace(/\//g, ""), 10) + 19110000;
+      // console.log('dt', dt);
+      d[0] = moment(dt, "YYYYMMDD").format("YYYY-MM-DD"); // 20221215 => 2022-12-15
+      d.unshift(stockNo);
 
-      // 111/12/15 => 2022-12-15
-      // 111/12/15 => 1111215 +19110000 => 20221215
-
-      let dt = parseInt (d[0].replace(/\//g, ""), 10) + 19110000;
-    //   console.log("dt", dt);
-      d[0] = moment(dt, "YYYYMMDD").format("YYYY-MM-DD");
-      d.unshift(stockNo)
-
-      let [result] = await connection.query('INSERT INTO stock_prices (stock_id, date, open_price, high_price, low_price, close_price, delta_price, transactions, volume, amount)',d)
+      let [result] = await connection.query(
+        "INSERT INTO stock_prices (stock_id, date, volume, amount, open_price, high_price, low_price, close_price, delta_price, transactions) VALUES (?,?,?,?,?,?,?,?,?,?)",
+        d
+      );
       console.log(result);
-      return d
+      return d;
+      // console.log('after', d);
     });
-
-    // console.log("await", response.data.data);
   } catch (e) {
     console.error(e);
   } finally {
