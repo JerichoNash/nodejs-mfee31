90f91e665b9e4ba03cf68d266ad9f3ef2b1f11a7
JerichoNash <jerichonash@gmail.com>
2022-12-17 12:03:31 +0800

    add readme.md,git add crawler4.js,env.example,test.js

--- crawler/test.js
+++ crawler/test.js
@@ -1,13 +1,16 @@
 // install mysql2
 const mysql2 = require("mysql2/promise");
-
+// npm i dotenv
+require('dotenv').config();
+// let dotenv = require('dotenv');
+// dotenv.config();
 (async () => {
   const connection = await mysql2.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "admin",
-    password: "12345",
-    database: "stock_mfee31",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
   });
 
   // simple query
@@ -15,7 +18,8 @@ const mysql2 = require("mysql2/promise");
   //   let data = result[0];
 
   //   改寫版本
-  const [data] = await connection.query("SELECT * FROM `stocks`");
+  let [data] = await connection.query("SELECT * FROM `stocks`");
+
   //   console.log(result);
   console.log(data);
 
